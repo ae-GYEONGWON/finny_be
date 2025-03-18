ad2a6a0ff25976a6205709fb3a53b5faa03be4b8
ae-GYEONGWON <tjsxor50@gmail.com>
2025-03-19 01:24:02 +0900

    비동기 엔진으로 변경

--- src/core/database.py
+++ src/core/database.py
@@ -1,8 +1,8 @@
 from typing import AsyncGenerator
 
-from sqlalchemy import create_async_engine
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.orm import AsyncSession, DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
 from src.core.config import get_setting
 
@@ -18,7 +18,9 @@ SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://{}:{}@{}:{}/{}".format(
 )
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
-factory = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+session_factory = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
 
 
 class Base(DeclarativeBase):
@@ -26,7 +28,7 @@ class Base(DeclarativeBase):
 
 
 async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
-    async with factory() as session:
+    async with session_factory() as session:
         try:
             yield session
         except SQLAlchemyError as error:
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.api import api_router
+from src.core.database import engine
 
 api = FastAPI(title="Virtual Trading System")
 
@@ -14,6 +15,13 @@ api.add_middleware(
     allow_methods=["*"],  # 모든 HTTP 메서드를 허용하려면 "*"
     allow_headers=["*"],  # 모든 HTTP 헤더를 허용하려면 "*"
 )
+
+
+@api.on_event("shutdown")
+async def shutdown():
+    print("Closing database connection...")
+    await engine.dispose()
+    print("Database connection closed.")
 
 
 api.include_router(api_router)
