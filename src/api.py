b6255f3e8a4da4ab687394bb0240e132edd01f8f
ae-GYEONGWON <tjsxor50@gmail.com>
2025-03-18 14:08:34 +0900

    폴더 이름 변경 view.py -> routes.py

--- src/api.py
+++ src/api.py
@@ -4,6 +4,11 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
 
+from src.auth.routes import router as auth_router
+from src.market.routes import router as market_router
+from src.orders.routes import router as orders_router
+from src.portfolio.routes import router as portfolio_router
+
 
 class ErrorMessage(BaseModel):
     msg: str
@@ -24,4 +29,8 @@ api_router = APIRouter(
     },
 )
 
-# api_router.include_router(snowball_router, tags=["snowball"]) # ex
+# 라우터 등록
+api_router.include_router(auth_router, prefix="/auth", tags=["Auth"])
+api_router.include_router(orders_router, prefix="/orders", tags=["Orders"])
+api_router.include_router(portfolio_router, prefix="/portfolio", tags=["Portfolio"])
+api_router.include_router(market_router, prefix="/market", tags=["Market"])
